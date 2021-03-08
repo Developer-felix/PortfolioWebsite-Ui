eabdd641aa6b77d00555d232cd06b6809ed3fd08
Developer-felix <onjombafelix1@gmail.com>
2021-03-08 09:23:29 -0800

    Changes on the description and added the test place

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger_icon = document.querySelector('#nav-bar .nav-bar .nav-list .hamburger');
-const nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
-
-hamburger_icon.addEventListener('click', () => {
-	nav_list.classList.toggle('open');
-	hamburger_icon.classList.toggle('active');
-});
